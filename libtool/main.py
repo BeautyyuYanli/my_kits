d27ae2ea04b86e3bcea7b99eb32f9a9b785583ae
beautyyuyanli <beautyyuyanli@outlook.com>
2021-03-11 15:15:53 +0800

    update libtool

--- libtool/main.py
+++ libtool/main.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time, sys
+from selenium.webdriver import Firefox as CustomDriver
+from seleniumrequests.request import RequestMixin
+from datetime import date
+from xvfbwrapper import Xvfb
+import time, sys, json, codecs
 def get_by_css(driver, cssstr, multi=0):
     for i in range(30):
     # while 1:
@@ -20,19 +19,6 @@ def get_by_css(driver, cssstr, multi=0):
         else:
             return element
     return 0
-def click_by_css(driver, cssstr):
-    while 1:
-    # for i in range(10):
-        try:
-            element = get_by_css(driver, cssstr)
-            driver.execute_script("$('" + cssstr + "').click()")
-            time.sleep(0.2)
-        except:
-            time.sleep(0.1)
-            pass
-        else:
-            # print('0')
-            return
 # delay time: depends on network
 d = 0.5
 # username ans password
@@ -40,25 +26,17 @@ username = sys.argv[2]
 passwd = sys.argv[3]
 # 伯川=1, 令希=2
 lib = sys.argv[1]
+vdis = Xvfb()
+vdis.start()
+class MyCustomWebDriver(CustomDriver, RequestMixin):
+    pass
+if (sys.platform == 'win32'):
+    driver = MyCustomWebDriver('chromedriver.exe')
+else:
+    driver = MyCustomWebDriver()
 
 if __name__ == '__main__':
-    if (sys.platform == 'win32'):
-        driver = webdriver.Chrome('chromedriver.exe')
-    else:
-        driver = webdriver.Chrome()
-        # driver = webdriver.Firefox()
-    driver.get("https://sso.dlut.edu.cn/cas/login?service=http://seat.lib.dlut.edu.cn/yanxiujian/client/login.php?redirect=index.php")
-    login_username = driver.find_element_by_class_name('person')
-    login_passwd = driver.find_element_by_class_name('lock')
-    login_username.send_keys(username)
-    login_passwd.send_keys(passwd)
-    login_passwd.send_keys(Keys.RETURN)
 
-    clicker = driver.find_element_by_partial_link_text('座位预约')
-    ActionChains(driver).click(clicker).perform()
-
-    time.sleep(1)
-    click_by_css(driver, 'tr:nth-child(' + lib + ') > td')
     def if_free(rooms):
         for i in rooms:
             if (int(i.text.split('剩余:')[1]) > 0):
@@ -69,10 +47,11 @@ if __name__ == '__main__':
     def get_a_free_room():
         cnt = 0
         while 1:
+            time.sleep(d)
             cnt += 1
             while 1:
                 try:
-                    rooms = get_by_css(driver, 'td', 1)
+                    rooms = get_by_css(driver, 'tbody>tr', 1)
                     rooms.pop(0)
                 except:
                     continue
@@ -84,43 +63,55 @@ if __name__ == '__main__':
             room = if_free(rooms)
             if (room != 0):
                 return room
-            time.sleep(d)
             driver.refresh()
             if (cnt % 30) == 0:
                 print("retry" + str(cnt))
     
-    def book_a_seat():
-        click_by_css(driver, 'button.btn-primary:nth-child(1)')
-        orderModal = get_by_css(driver, '#orderModal')
-        # ---- begin check if went to next step ----
-        while 1:
-            try:
-                time.sleep(0.1)
-                notification = driver.find_element_by_css_selector('div.bootbox-body')
-            except:
-                # if orderModal.value_of_css_property('display') != 'none':
+    def book_a_seat(room_id):
+        date_str = date.today().strftime('%Y/%m/%d')
+        res = driver.request('POST', 'http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=randomRoomSeatChoose', data = {'room_id': str(room_id), 'order_date': date_str})
+        if res.status_code == 200:
+            res_json = json.loads(codecs.decode(res.text.encode(), 'utf-8-sig')) 
+            if res_json['success']:
                 code = 1
-                break
             else:
                 code = 0
-                break
-        # ---- end check if went to next step ----
+        else:
+            code = 0
+
         if code == 1:
-            click_by_css(driver, '#btn_submit_addorder')
-            result = get_by_css(driver, 'div.bootbox-body')
-            if (result == 0):
-                code = 1
+            post_data = {'addCode': res_json['data']['addCode']}
+            res = driver.request('POST', 'http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=addSeatOrder', data = post_data)
+            res_json = json.loads(codecs.decode(res.text.encode(), 'utf-8-sig')) 
+            if res.status_code and res_json['success']:
+                pass
             else:
                 code = 0
         return code
-    
+    # login
+    driver.get("https://sso.dlut.edu.cn/cas/login?service=http://seat.lib.dlut.edu.cn/yanxiujian/client/login.php?redirect=index.php")
+    login_username = driver.find_element_by_class_name('person')
+    login_passwd = driver.find_element_by_class_name('lock')
+    login_username.send_keys(username)
+    login_passwd.send_keys(passwd)
+    login_passwd.send_keys(Keys.RETURN)
+    time.sleep(d * 2)
+    # get list
+    if lib == '1':
+        driver.get("http://seat.lib.dlut.edu.cn/yanxiujian/client/roomSelectSeat.php?area_id=17")
+    elif lib == '2':
+        driver.get("http://seat.lib.dlut.edu.cn/yanxiujian/client/roomSelectSeat.php?area_id=32")
+    time.sleep(d * 2)
+    # check list
     while 1:
-        get_a_free_room().click()
-        code = book_a_seat()
+        free_room = get_a_free_room()
+        room_id = free_room.get_attribute('data-uniqueid')
+        code = book_a_seat(room_id)
         if code == 0:
             driver.refresh()
             print('false!')
         else:
             driver.close()
+            vdis.stop()
             print('success!')
             break
