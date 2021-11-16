35ae8e56ce7dc0dec24ccd8e9118d4b2c7d4412e
beautyyuyanli <beautyyuyanli@outlook.com>
2021-11-16 10:14:20 +0800

    add debug info

--- libtool/new.py
+++ libtool/new.py
@@ -30,7 +30,6 @@ def get_a_free_room():
         delt = math.floor(datetime.datetime.now().timestamp() * 1000)
         r = s.get('http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=roomList&area_id={}&order_date={}&_={}'.format(libId, order_date, delt))
         l = r_json(r)
-
         for i in l:
             if i['room_name'].find('111') == -1 and i['room_name'].find('临时') == -1:
                 if int(i['en_num']) > int(i['use_num']):
@@ -73,29 +72,31 @@ if mode == '1' or mode == '2':
 def book_advanced_seat():
     order_date = datetime.date.today().strftime('%Y/%m/%d')
     delt = math.floor((datetime.datetime.now() + datetime.timedelta(days=1)).timestamp() * 1000)
-    r = s.get('http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=querySeatMap&order_date={}&room_id=199&_={}'.format(order_date, delt))
+    r = s.get('http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=querySeatMap&order_date={}&room_id=199'.format(order_date))
     l = r_json(r)
     advanced_seats = list(range(133,141)) + list(range(157,165)) + list(range(281, 289)) + \
         list(range(256,264)) + list(range(248,256)) + list(range(224, 232)) + \
         list(range(216,224))
     for i in l:
         for j in i:
-            if int(j['seat_label']) in advanced_seats and j['seat_type'] == '1':
-                postdate = 'seat_id={}&order_date={}'.format(j['seat_id'], order_date)
-                r = s.post('http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=seatChoose', postdate)
-                ll = r_json(r)
-                if ll['success']:
-                    post_data = {'addCode': ll['data']['addCode'], 'method': 'addSeat'}
-                    r = s.post('http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=addSeatOrder', post_data)
-                    lll = r_json(r)
-                    if lll['success']:
-                        return 1
+            if int(j['seat_label']) in advanced_seats :
+                print(j['seat_label'], int(j['seat_type']), int(j['seat_order_status']))
+                if int(j['seat_type']) == 1:
+                    postdate = 'seat_id={}&order_date={}'.format(j['seat_id'], order_date)
+                    r = s.post('http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=seatChoose', postdate)
+                    ll = r_json(r)
+                    if ll['success']:
+                        post_data = {'addCode': ll['data']['addCode'], 'method': 'addSeat'}
+                        r = s.post('http://seat.lib.dlut.edu.cn/yanxiujian/client/orderRoomAction.php?action=addSeatOrder', post_data)
+                        lll = r_json(r)
+                        if lll['success']:
+                            return 1
+                        else:
+                            print(lll)
                     else:
-                        print(lll)
-                else:
-                    print(ll)
-                print(j['seat_label'], 'false')
-                time.sleep(0.5)
+                        print(ll)
+                    print(j['seat_label'], 'false')
+                    time.sleep(0.5)
     return 0
 
 if mode == '3':
